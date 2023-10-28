2aed277fa3f9290267d83764f57a7db5a736ade9
unknown <khushi.jaduvanshi@gmail.com>
2023-10-29 03:51:05 +0530

    Configuring all routes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,63 @@
 require("dotenv").config();
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
+
+// express middleware
+app.use(express.json());
+
 const port = process.env.PORT || 3006;
 
-app.get("/getClinics",(request, response) => {
-    response.json({
+// get all clinics
+app.get("/api/v1/clinics",(request, response) => {
+    response.status(200).json({
         status: "success",
-        clinic: "Pet Care"
+        data: {
+            clinic: ["Pet Care","Furry"]
+        }
+        
     });
 }); 
 
+// get a clinic
+app.get("/api/v1/clinics/:id",(request, response) => {
+    response.status(200).json({
+        status: "success",
+        data: {
+            clinic: ["Pet Care","Furry"]
+        }
+        
+    });
+}); 
+
+// create a clinic
+app.post("/api/v1/clinics",(request, response) => {
+    response.status(201).json({
+        status: "success",
+        data: {
+            clinic: ["Pet Care","Furry"]
+        }
+        
+    });
+}); 
+
+// update a clinic
+app.put("/api/v1/clinics/:id",(request, response) => {
+    response.status(200).json({
+        status: "success",
+        data: {
+            clinic: ["Pet Care","Furry"]
+        }
+        
+    });
+}); 
+
+// delete a clinic
+app.delete("/api/v1/clinics/:id",(request, response) => {
+    response.status(204).json({
+        status: "success",        
+    });
+}); 
 
 app.listen(port, () => {
     console.log(`Server is up and listening on port ${port}`);
