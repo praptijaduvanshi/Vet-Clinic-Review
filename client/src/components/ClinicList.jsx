b1587dac0529da662b7a7ade90707ab6b9a32c8f
unknown <khushi.jaduvanshi@gmail.com>
2023-10-29 21:58:02 +0530

    Finished homepage components

--- client/src/components/ClinicList.jsx
+++ client/src/components/ClinicList.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 
 const ClinicList = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="list-group" style={{ margin: '7px' }}>
+      <table className="table table-hover table-bordered">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col"> Clinic Name</th>
+            <th scope="col"> Address</th>
+            <th scope="col"> City</th>
+            <th scope="col"> State</th>
+            <th scope="col"> Open Hours</th>
+            <th scope="col"> Phone No.</th>
+            <th scope="col"> Email</th>
+            <th scope="col"> Established On</th>
+            <th scope="col"> Ratings</th>
+            <th scope="col"> Edit</th>
+            <th scope="col"> Delete</th>
+          </tr>
+        </thead>
 
-export default ClinicList
+        <tbody>
+          <tr>
+            <td>A</td>
+            <td>A</td>
+            <td>A</td>
+            <td>A</td>
+            <td>A</td>
+            <td>A</td>
+            <td>A</td>
+            <td>A</td>
+            <td>A</td>
+            <td><button className="btn btn-info">Edit</button></td>
+            <td><button className="btn btn-danger">Delete</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ClinicList;
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import AddClinic from '../components/AddClinic';
+import ClinicList from '../components/ClinicList';
 
 const Home = () => {
   return (
     <div>
         <Header/>
         <AddClinic />
+        <ClinicList />
     </div>
   )
 }
